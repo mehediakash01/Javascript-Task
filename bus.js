d76972d864652733b2a2d5fe6212edaf09655a81
ronSiuuuu <mehedihasanakash1717@gmail.com>
2025-02-06 15:08:21 +0600

    completed some js basic task

---
+++ bus.js
@@ -0,0 +1,27 @@
+/***
+
+Ticket fare Calculator
+    - Children (age < 10): free
+    - Students get a 50% discount
+    - Senior citizens (age >= 60) gets a 15% Discount
+    - Otherwise Regular ticket fare 800 tk
+*/
+
+const age = 25;
+let ticket =800;
+const Students = true;
+if (age<10){
+    console.log("Free");
+} 
+else if (Students && age<=25){
+    let discount = ticket*50/100;
+
+    console.log(ticket-discount);
+}
+else if (age>=60){
+  let  discount = ticket*15/100;
+    console.log(ticket-discount);
+}
+else{
+    console.log(ticket);
+}
